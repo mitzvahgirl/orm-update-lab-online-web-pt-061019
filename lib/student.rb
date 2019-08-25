e14fabdc81b5a9eb29fbec9734913f9abed1b5c8
Amy Fogelman <abfnj1@gmail.com>
2019-08-25 04:55:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,3 @@
-require_relative "../config/environment.rb"
-
-class Student
-  attr_accessor :id, :name, :grade
-    
-  def initialize(id=nil, name, grade)
-    @id, @name, @grade = id, name, grade
-  end
-  
  require_relative "../config/environment.rb"
 
 class Student
